abbcbc764481c68b7aaebb27d459ce4cdd8dedb3
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-09 19:57:35 +0200

    chore: add first program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,7 +4,7 @@
     /// <summary>
     /// This is the first program in C# that prints "Hello, World!" to the console. 
     /// It serves as a simple introduction to C# programming and demonstrates the basic structure of a C# application, 
-    /// including the use of namespaces, classes, and the Main method as the entry point of the program.
+    /// including the use of namespaces, classes, and the Main method as the entry point of the program. 
     /// </summary>
 
 
@@ -12,8 +12,9 @@
     {
         static void Main(string[] args)
         {
+            
             Console.WriteLine("Hello, World!");
-            Console.WriteLine();
+            
         }
     }
 }
