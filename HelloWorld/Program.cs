e631bf8e4bdacd0ca9c189dec8a1e8284ac6e52f
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-09 19:22:00 +0200

    feat: add documentation comments

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace HelloWorld
 {
+
+    /// <summary>
+    /// This is the first program in C# that prints "Hello, World!" to the console. 
+    /// It serves as a simple introduction to C# programming and demonstrates the basic structure of a C# application, 
+    /// including the use of namespaces, classes, and the Main method as the entry point of the program.
+    /// </summary>
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine();
         }
     }
 }
