da9de61cf87420a558fca05ea81a929146b261b5
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-09 19:58:33 +0200

    chore: add console print logic

---
+++ PrintingApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrintingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+
+            Console.WriteLine("Num = {0}", num);    // placeholder
+            Console.WriteLine($"Num = {num}");      // interpolation
+
+
+        }
+    }
+}
