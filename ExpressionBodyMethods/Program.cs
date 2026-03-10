0380a95976007de65bef6478ae81f3133972c0fc
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-10 20:37:13 +0200

    chore: add expression body methods

---
+++ ExpressionBodyMethods/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ExpressionBodyMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+
+        //Expression-bodied method for a simple addition operation
+        public static int Add(int a, int b) => a + b;
+
+        public static int Factorial(int n) => n == 0 ? 1 : n * Factorial(n - 1);
+
+        public static bool IsPrime(int n) => n >= 2 && Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i != 0);
+    }
+}
