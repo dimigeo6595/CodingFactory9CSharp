6393693dcf85acf064b68105e3787e0ec7c3159e
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-11 20:10:20 +0200

    chore: add palindrome app for strings manipulation

---
+++ PalindromeStr/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PalindromeStr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        public static bool IsPalindrome(string? s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+            {
+                if (s[i] != s[j])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
