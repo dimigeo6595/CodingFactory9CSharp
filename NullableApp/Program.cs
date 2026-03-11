0a42348707342c4f38bee3a02670c00d6fc79185
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-11 20:09:48 +0200

    chore: add nullable strings

---
+++ NullableApp/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NullableApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String? s = Console.ReadLine(); // posible null value
+
+            if (s != null) // null check
+            {
+                Console.WriteLine(s.Length);
+            }
+
+
+            Console.WriteLine(s?.Length); // null conditional operator
+            Console.WriteLine(s!.Length); // null-forgiving operator, tells the compiler that s is not null, but can throw a NullReferenceException at runtime if s is actually null
+            Console.WriteLine(s ?? "default"); // null-coalescing operator, returns the value of s if it is not null, otherwise returns "default"
+        }
+    }
+}
