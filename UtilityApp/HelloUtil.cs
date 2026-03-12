2b9d2048ec5257c33f5e0ea82f8753ca1e6ffcae
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-12 20:50:48 +0200

    chore: add utility class logic with classes and public statid methods

---
+++ UtilityApp/HelloUtil.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UtilityApp
+{
+    internal static class HelloUtil
+    {
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void SayGoodbye(string name)
+        {
+            Console.WriteLine($"Goodbye, {name}!");
+        }
+
+
+
+    }
+}
