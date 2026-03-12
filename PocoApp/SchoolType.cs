94bc47ae93ffa7cede62dd54a29c85d3f2a8b8c3
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-12 20:53:21 +0200

    chore: add StudentInsertDTO logic and shool type enum

---
+++ PocoApp/SchoolType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PocoApp
+{
+    internal enum SchoolType
+    {
+        IEK,    //irdinal 0
+        KEK,    //ordinal 1
+        EPAS    //ordinal 2
+    }
+}
