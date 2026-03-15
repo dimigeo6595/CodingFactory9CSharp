dba07fe2b91393988a8a7715e84cf06838e7e4e1
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-15 14:47:28 +0200

    chor: add point class

---
+++ PointApp/Model/Point.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PointApp.Model
+{
+    internal class Point
+    {
+        public int X { get; set; }
+        
+
+        public Point() 
+        { 
+
+        }
+
+        public Point(int x) 
+        {
+            X = x; 
+        }
+
+        public override string ToString() => $"Point(X: {X})";
+
+        public virtual void Move5() // virtual method that can be overridden in derived classes
+        {
+            X += 5;
+        }
+
+
+    }
+}
