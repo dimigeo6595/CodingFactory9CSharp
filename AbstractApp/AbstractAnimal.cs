c47982eca2623a84cf14f6f5d552fe5082612fbc
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-15 16:28:46 +0200

    chore: add abstract class logic

---
+++ AbstractApp/AbstractAnimal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractApp
+{
+
+    /// <summary>
+    /// Abstract classes are also virtual.
+    /// Override is used only virtual or abstract classes. 
+    /// </summary>
+    internal abstract class AbstractAnimal
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public double Age { get; set; }
+
+        public abstract override string ToString();
+
+        public virtual void Eat()
+        {
+            Console.WriteLine($"{Name} is eating.");
+        }
+
+
+
+    }
+}
---
+++ AbstractApp/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbstractApp
+{
+    internal class Cat : AbstractAnimal
+    {
+        public override void Eat()
+        {
+            base.Eat();
+            Console.WriteLine($"{Name} is finally eating.");
+        }
+
+        public override string ToString() => $"Cat: Id={Id}, Name={Name}, Age={Age}";
+
+    }
+}
