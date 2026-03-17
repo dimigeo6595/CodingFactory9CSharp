42f034d51625468874ac1eb0503efb206fbeb74b
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-17 19:29:00 +0200

    chore: add FileManagement app with listnode and dllist

---
+++ FileManagement/ListNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileManagement
+{
+    internal class ListNode<T>
+    {
+        public T? Value{ get; set; }
+        public int Count { get; set; }
+
+
+    }
+}
