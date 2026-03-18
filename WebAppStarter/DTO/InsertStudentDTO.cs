b67f21f8eb19814fe6575791c49ffe9afc423e81
GiorgosDimitriadis <dimigeo6595@gmail.com>
2026-03-18 19:25:07 +0200

    add index controller

---
+++ WebAppStarter/DTO/InsertStudentDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppStarter.DTO
+{
+    public record InsertStudentDTO
+    (
+        [property: Required(ErrorMessage = "Firstname is required")]
+        [property: MinLength(1, ErrorMessage = "Firstname must not be empty")]
+        string? Firstname,
+
+        [property: Required(ErrorMessage = "Firstname is required")]
+        [property: MinLength(1, ErrorMessage = "Firstname must not be empty")]
+        string? Lastname,
+
+        int SelectedCityId
+    )
+    {
+        
+        public InsertStudentDTO() : this(null, null, 0)
+        {
+        }
+
+    }
+}
---
+++ WebAppStarter/DTO/StudentReadOnlyDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebAppStarter.DTO
+{
+    public record StudentReadOnlyDTO(int Id, string? Firstname, string? Lastname)
+    {
+        public StudentReadOnlyDTO() : this(0, null, null)
+        {
+        }
+
+    }
+    
+}
